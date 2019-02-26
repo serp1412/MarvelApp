f9d53b31c2c6b125ad5b7d415d56424e9fb46e82
Serghei Catraniuc <serghei.catraniuc@yopeso.com>
2019-02-26 22:17:05 +0200

    Improve Thumbnail

--- MarvelApp.playground/Contents.swift
+++ MarvelApp.playground/Contents.swift
@@ -1,11 +1,16 @@
 import Foundation
+import UIKit
+import PlaygroundSupport
 @testable import MarvelAppFramework
 
 AppEnvironment.current.api.getHeroes { (result) in
     switch result {
     case .success(let heroes): print(heroes)
+      let data = try! Data(contentsOf: heroes.first!.thumbnail.url())
+    DispatchQueue.main.async {
+        let imageView = UIImageView(image: UIImage(data: data))
+        PlaygroundPage.current.liveView = imageView
+    }
     case .failure: print("Failed")
     }
 }
-
-RunLoop.main.run()
--- Thumbnail.swift
+++ Thumbnail.swift
@@ -1,4 +1,28 @@
+import Foundation
+
+enum ImageType {
+    case portrait
+    case landscape
+    case square
+    case fullsize
+
+    var string: String {
+        switch self {
+
+        case .portrait: return "/portrait_fantastic"
+        case .landscape: return "/landscape_xlarge"
+        case .square: return "/standard_xlarge"
+        case .fullsize: return ""
+        }
+    }
+}
+
 struct Thumbnail: Codable {
     private let path: String
     private let `extension`: String
+
+    func url(for type: ImageType = .square) -> URL {
+        let urlString = path + type.string + "." + self.extension
+        return URL(string: urlString)!
+    }
 }
