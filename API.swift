3e453aaf219cac2f2fb4d3e80c9505e0f00860b0
Serghei Catraniuc <serghei.catraniuc@yopeso.com>
2019-03-02 16:16:48 +0200

    Refactor HeroProductsRequest

--- API.swift
+++ API.swift
@@ -18,29 +18,26 @@ class API: APIType {
     }
 
     func getComics(heroId: Int, limit: Int = 0, completion: @escaping (Result<APICollection<HeroProduct>>) -> ()) {
-        let request: ComicsRequest = .init(options: .heroId(heroId), limit > 0 ? .limit(limit) : nil)
-
-        perform(request: request, completion: completion)
+        getHeroProducts(kind: .comics, heroId: heroId, completion: completion)
     }
 
     func getEvents(heroId: Int, limit: Int = 0, completion: @escaping (Result<APICollection<HeroProduct>>) -> ()) {
-        let request: EventsRequest = .init(options: .heroId(heroId), limit > 0 ? .limit(limit) : nil)
-
-        perform(request: request, completion: completion)
+        getHeroProducts(kind: .events, heroId: heroId, completion: completion)
     }
 
     func getStories(heroId: Int, limit: Int = 0, completion: @escaping (Result<APICollection<HeroProduct>>) -> ()) {
-        let request: StoriesRequest = .init(options: .heroId(heroId), limit > 0 ? .limit(limit) : nil)
-
-        perform(request: request, completion: completion)
+        getHeroProducts(kind: .stories, heroId: heroId, completion: completion)
     }
 
     func getSeries(heroId: Int, limit: Int = 0, completion: @escaping (Result<APICollection<HeroProduct>>) -> ()) {
-        let request: SeriesRequest = .init(options: .heroId(heroId), limit > 0 ? .limit(limit) : nil)
+        getHeroProducts(kind: .series, heroId: heroId, completion: completion)
+    }
+
+    private func getHeroProducts(kind: HeroProductsRequest.Kind, heroId: Int, limit: Int = 0, completion: @escaping (Result<APICollection<HeroProduct>>) -> ()) {
+        let request: HeroProductsRequest = .init(kind: kind, options: .heroId(heroId), limit > 0 ? .limit(limit) : nil)
 
         perform(request: request, completion: completion)
     }
-
 
     private func perform<R: RequestType>(request: R,
                                          completion: @escaping (Result<R.ResponseType>) -> ()) {
---
+++ HeroProductsRequest.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+struct HeroProductsRequest: RequestType {
+    typealias ResponseType = APICollection<HeroProduct>
+    let endpoint: String
+    let options: [String]
+
+    enum Kind {
+        case comics
+        case series
+        case stories
+        case events
+
+        var endpoint: String {
+            switch self {
+            case .comics: return "/comics?"
+            case .series: return "/series?"
+            case .stories: return "/stories?"
+            case .events: return "/events?"
+            }
+        }
+    }
+
+    enum Options {
+        case heroId(Int)
+        case limit(Int)
+
+        var string: String {
+            switch self {
+            case .limit(let limit): return "limit=\(limit)"
+            case .heroId(let id): return "characters=\(id)"
+            }
+        }
+    }
+
+    init(kind: Kind, options: [Options] = []) {
+        self.endpoint = kind.endpoint
+        self.options = options.map { $0.string }
+    }
+
+    init(kind: Kind, options: Options?...) {
+        self.endpoint = kind.endpoint
+        self.options = options.compactMap { $0?.string }
+    }
+}
